703ce75c18cded01531e02337fb4ae09bf74b14f
Tanya Shrivastava <140186118+tanyashrivastava21@users.noreply.github.com>
2025-07-07 11:01:29 +0530

    Update app_py.py

--- app_py.py
+++ app_py.py
@@ -7,7 +7,7 @@ Original file is located at
     https://colab.research.google.com/drive/1bGQjLdw0K7KQt9D75nn9eh6exaOAcjwk
 """
 
-pip install streamlit
+
 
 import streamlit as st
 import numpy as np
